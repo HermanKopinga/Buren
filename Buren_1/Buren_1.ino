057028335c0a99b6c8147bd6d19e91f501aa1822
Herman Kopinga <herman@kopinga.nl>
2015-07-09 20:15:01 +0200

    New packet layout & beautification.
    /*
     1 time (2 bytes) start time for color (fraction: 8 bits.8 bits
    0-255.0-255)
     2 Frequency (byte) freq for color calcualtion / 100
     3 Broad (byte) color calculation
     4 Center (byte) range of color calculation
     5 Intensity (byte) start intensity led
     6 ColorShift (byte) Hoeveel veranderd de kleur tussen pixies / 100
     7 FadeOut (byte) Speed to fade the led across pixies to black =0 ook
    stop passing on
     8 FadeSpeedIn (byte) Speed to fade in the led inside the pixie
     9 FadeSpeedOut (byte) Speed to fade out led inside pixie
     10 Rest (byte) stop listening after sending
     11 RestSpeed (signed byte) to speed up or slow down sending
     12 Future (byte) future expansion
     13 Cricket (bit) turn on cricket
     15 BatteryLevel (bit) show us your batt level
     14 Character (4 bits) Caracter (its an nibble)
     15 FindMyPixi (bit) show were you are
     16 MonkyLives (bit) got mental!!!
     */

--- Buren_1/Buren_1.ino
+++ Buren_1/Buren_1.ino
@@ -3,37 +3,43 @@
 #include "RF24.h" // rf lib
 #include <Adafruit_DotStar.h>
 #include "printf.h"// used for debuggin the radio.print
-/*1 TimeIn = Byte start time for color
-2 Frequentie = Byte freq for color calcualtion
-3 Broad = Byte  color calculation
-4 Center =  Byte range of color calculation
-5 Intensity = start intensity led
-6 ColorShift = Byte Hoeveel veranderd de kleur tussen pixies 
-7 FadeOut = Byte Speed to fade the led across pixies to black =0 ook stop passing on
-8 FadeSpeedIn = Byte Speed to fade in the led inside the pixie
-9 FadeSpeedOut = Byte Speed to fade out led inside pixie
-10 Rest = Byte stop listening after sending
-11 RestSpeed = Singt bite to speed up or slow down sending
-12  Future     =  Byte future expansion
-13  Cricket = Bit turn on cricket
-15 BatteryLevel = Bit show us your batt level
-14 Character = 4bits Caracter (its an nibble)
-15 FindMyPixi = Bit show were you are
-16 MonkyLives  = Bit got mental!!!
-*/
+
+/*
+ 1 time (2 bytes) start time for color (fraction: 8 bits.8 bits 0-255.0-255)
+ 2 Frequency (byte) freq for color calcualtion / 100
+ 3 Broad (byte) color calculation
+ 4 Center (byte) range of color calculation
+ 5 Intensity (byte) start intensity led
+ 6 ColorShift (byte) Hoeveel veranderd de kleur tussen pixies / 100
+ 7 FadeOut (byte) Speed to fade the led across pixies to black =0 ook stop passing on
+ 8 FadeSpeedIn (byte) Speed to fade in the led inside the pixie
+ 9 FadeSpeedOut (byte) Speed to fade out led inside pixie
+ 10 Rest (byte) stop listening after sending
+ 11 RestSpeed (signed byte) to speed up or slow down sending
+ 12 Future (byte) future expansion
+ 13 Cricket (bit) turn on cricket
+ 15 BatteryLevel (bit) show us your batt level
+ 14 Character (4 bits) Caracter (its an nibble)
+ 15 FindMyPixi (bit) show were you are
+ 16 MonkyLives (bit) got mental!!!
+ */
 
 // PIXI dust
-#define PAYLOAD_SIZE 9
+#define PAYLOAD_SIZE 14
 
-byte red = 0;
-byte green = 0;
-byte blue = 0;
+float time = 0;
+float frequency = 0;
+byte broad = 0;
+byte center = 0;
+byte intensity = 0;
+float colorShift = 0;
+byte fadeOut = 0;
+byte fadeSpeedIn = 0;
+byte fadeSpeedOut = 0;
 byte rest = 0;
-byte mutation = 0;
-byte lifeTime = 0;
-byte colorShift = 0;
+byte restSpeed = 0;
 byte future = 0;
-byte thingies = 0;
+byte bitField = 0;
 bool findMyPixi = 0;
 bool batteryLevel = 0;
 byte character = 0;
@@ -42,18 +48,16 @@ byte buffer[PAYLOAD_SIZE];
 
 
 // piezo hight
-int low =10;
+int low = 10;
 
 // variables voor color shift
-float frequency = .3; // color wheel
-float time; // color wheel
 float redshift;
 float greenshift;
 float blueshift;
-byte redB = 255;
-byte blueB= 0;
-byte greenB= 0;
-// bateri life
+byte red = 0;
+byte green = 0;
+byte blue = 0;
+// battery life
 
 //
 // Hardware configuration
@@ -73,72 +77,67 @@ const uint64_t pipe = 0xF0F0F0F0E1LL;
 
 
 void setup(void){
-  
-Serial.begin(9600);
-printf_begin();  // strart the print.h
-pinMode(speakerPin, OUTPUT);
-// beging seq led test
-strip.begin(); 
-strip.setPixelColor(0, strip.Color(255, 255, 255));
-strip.show();
-delay(100);
-strip.setPixelColor(1, strip.Color(255, 255, 255));
-strip.show();
-delay(100);
-strip.setPixelColor(2, strip.Color(255, 255, 255));
-strip.show();
-delay(100);
-strip.setPixelColor(0, strip.Color(0, 0, 0));
-strip.setPixelColor(1, strip.Color(0, 0, 0));
-strip.setPixelColor(2, strip.Color(0, 0, 0));
-strip.show();
-// end led test
 
+  Serial.begin(9600);
+  printf_begin();  // strart the print.h
+  pinMode(speakerPin, OUTPUT);
+  // beging seq led test
+  strip.begin(); 
+  strip.setPixelColor(0, strip.Color(255, 255, 255));
+  strip.show();
+  delay(100);
+  strip.setPixelColor(1, strip.Color(255, 255, 255));
+  strip.show();
+  delay(100);
+  strip.setPixelColor(2, strip.Color(255, 255, 255));
+  strip.show();
+  delay(100);
+  strip.setPixelColor(0, strip.Color(0, 0, 0));
+  strip.setPixelColor(1, strip.Color(0, 0, 0));
+  strip.setPixelColor(2, strip.Color(0, 0, 0));
+  strip.show();
+  // end led test
 
+  // Initialize all pixels to 'off'
 
-
- // Initialize all pixels to 'off'
-  
-radio.begin(); // start rf
-// other radio setups:
+  radio.begin(); // start rf
+  // other radio setups:
   radio.setRetries(15,15); // amount of retries
   radio.setPALevel(RF24_PA_MIN); // range level
   radio.setAutoAck(0); // dont wait for responce
   radio.setDataRate(RF24_250KBPS); // set the datarate
   radio.setPayloadSize(PAYLOAD_SIZE); // amout of Bytes sending
-  
+
   // Open pipes to other nodes for communication
   //
   radio.openReadingPipe(0,pipe);
   radio.openWritingPipe(pipe);  
-  
-// Start listening
-  radio.startListening();
- 
-// Serial prints
- radio.printDetails();
- // printf("lisening");
 
- }
- 
- void loop(void)
+  // Start listening
+  radio.startListening();
+
+  // Serial prints
+  radio.printDetails();
+  // printf("lisening");
+
+}
+
+void loop(void)
 {
   //
   // Pong back role.  Receive each packet, dump it out, and send it back
   //
 
   // if there is data ready
-  if ( radio.available() )
-  {
+  if ( radio.available() ) {
     // Dump the payloads until we've gotten everything
     unsigned long got_time;
     bool done = false;
 
-    while (!done)
-    {
+    while (!done) {
       // Fetch the payload, and see if this was the last one.
       done = radio.read(&buffer, PAYLOAD_SIZE);
-      
+
       // Spew it
       Serial.print("Got payload ");
       for (int i = 0; i < PAYLOAD_SIZE; i++) {
@@ -146,60 +145,56 @@ radio.begin(); // start rf
         Serial.print(": ");
         Serial.println(buffer[i], BIN);  
       }
-      
+
       // Serial.println (readVcc(), DEC);
       readVcc();
       // Delay just a little bit to let the other unit
       // make the transition to receiver
       delay(20);
     }
-  
-    red = buffer[0];
-    green = buffer[1]; 
-    blue = buffer[2];
-    rest = buffer[3];
-    mutation = buffer[4];
-    lifeTime = buffer[5];
-    colorShift = buffer[6];
-    future = buffer[7];
-    thingies = buffer[8];
-    findMyPixi = (thingies & 0x2) >> 1;
-    batteryLevel = (thingies & 0x40) >> 6;
-    monkeyLives = (thingies & 0x1);
-    character = (thingies & 0x3c) >> 2;
 
-    
-//  digitalWrite(ledPin, HIGH);
-//  strip.setPixelColor(0, strip.Color(255, 255, 255));
-//  strip.setPixelColor(1, strip.Color(255, 255, 255));
-//  
-//   strip.show();
-//   
+    time = buffer[0] + buffer [1]/100;
+    frequency = buffer[2] / 100; 
+    broad = buffer[3];
+    center = buffer[4];
+    intensity = buffer[5];
+    colorShift = buffer[6] / 100;
+    fadeOut = buffer[7];
+    fadeSpeedIn = buffer[8];
+    fadeSpeedOut = buffer[9];
+    rest = buffer[10];
+    restSpeed = buffer[11]; // Signed??
+    future = buffer[12];
+    bitField = buffer[13];
+    findMyPixi = (bitField & 0x2) >> 1;
+    batteryLevel = (bitField & 0x40) >> 6;
+    monkeyLives = (bitField & 0x1);
+    character = (bitField & 0x3c) >> 2;
 
     rainbow();
-    makeColor(red,green,blue);
-   // cricked();// CHIRP 
+    makeColor();
+    // cricked();// CHIRP 
     // First, stop listening so we can talk
     radio.stopListening();
 
     delay(rest*10);
-    makeColor(0,0,0);
-//   digitalWrite(ledPin, LOW);
-//    strip.setPixelColor(0, strip.Color(0, 0, 0));
-//    strip.setPixelColor(1, strip.Color(0, 0, 0));
-//    strip.show();    
 
     // Prepare buffer for transmit.
-    thingies = batteryLevel << 6 | (character & 0x0F) << 2 | findMyPixi << 1 | monkeyLives;  
-    buffer[0] = red;
-    buffer[1] = green; 
-    buffer[2] = blue;
-    buffer[3] = rest;
-    buffer[4] = mutation;
-    buffer[5] = lifeTime;
-    buffer[6] = colorShift;
-    buffer[7] = future;
-    buffer[8] = thingies;
+    bitField = batteryLevel << 6 | (character & 0x0F) << 2 | findMyPixi << 1 | monkeyLives;  
+    buffer[0] = int(time);         // Only the whole number of time
+    buffer[1] = time-(long)time;   // Only the fraction of time
+    buffer[2] = frequency * 100;   // Loses precision, no problem :)
+    buffer[3] = broad;
+    buffer[4] = center;
+    buffer[5] = intensity;
+    buffer[6] = colorShift * 100;  // Loses precision, no problem :)
+    buffer[7] = fadeOut;
+    buffer[8] = fadeSpeedIn;
+    buffer[9] = fadeSpeedOut;
+    buffer[10] = rest;
+    buffer[11] = restSpeed;
+    buffer[12] = future;
+    buffer[13] = bitField;    
     Serial.println("\nNow sending ");
     for (int i = 0; i < PAYLOAD_SIZE; i++) {
       Serial.print(i);
@@ -210,7 +205,7 @@ radio.begin(); // start rf
 
     // Send the final one back.
     radio.write(&got_time, sizeof(unsigned long));
-    
+
     Serial.print("Sent response.\n\r");
     delay(rest*10+200);
     // Now, resume listening so we catch the next packets.
@@ -218,14 +213,12 @@ radio.begin(); // start rf
   }
 }
 
- 
-void makeColor(byte red,byte green, byte blue) {
-  
-  for (int i = 0; i < NUMPIXELS; i++ ){
+
+void makeColor() {
+  for (int i = 0; i < NUMPIXELS; i++ ) {
     strip.setPixelColor(i, strip.Color(red, green, blue));
     strip.show();
   }     
-  
 }
 
 
@@ -248,34 +241,33 @@ long readVcc() {
 void rainbow() { 
   //http://krazydad.com/tutorials/makecolors.php
   time = time++;
-  red = sin(frequency* time + 0 +redB) * 127 + 128;
-  green = sin(frequency* time + 2+greenB) * 127 + 128;
-  blue  = sin(frequency* time + 4+blueB) * 127 + 128;
- 
- Serial.print(red);
- Serial.print("red");
- Serial.print(green);
- Serial.print("green"); 
- Serial.print(blue);
- Serial.print("blue");
+
+  Serial.print(red);
+  Serial.print("red");
+  Serial.print(green);
+  Serial.print("green"); 
+  Serial.print(blue);
+  Serial.print("blue");
   //if (time > 64.0){
-    //time=0;
- // }
+  //time=0;
+  // }
 }
- 
- /// piezo speaker kreak
- void cricked(){ 
-// tone(speakerPin, 200, 1000);
-    analogWrite(speakerPin, 255);
-    delay(low);
-    analogWrite(speakerPin, 0);
-    delay(low*random(2  ));
-    analogWrite(speakerPin, 255);
-    delay(low*random(2));
-    analogWrite(speakerPin, 0);
-    delay(low*random(10));
-    analogWrite(speakerPin, 255);
-    delay(low);
-    analogWrite(speakerPin, 0);
- }
- 
+
+/// piezo speaker kreak
+void cricked() { 
+  // tone(speakerPin, 200, 1000);
+  analogWrite(speakerPin, 255);
+  delay(low);
+  analogWrite(speakerPin, 0);
+  delay(low*random(2  ));
+  analogWrite(speakerPin, 255);
+  delay(low*random(2));
+  analogWrite(speakerPin, 0);
+  delay(low*random(10));
+  analogWrite(speakerPin, 255);
+  delay(low);
+  analogWrite(speakerPin, 0);
+}
+
+
+
