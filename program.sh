3f8a5bf05aa1cc1523f1934eea37c3fc50be3732
Herman Kopinga <herman@kopinga.nl>
2015-07-10 22:36:25 +0200

    less machine dependent.

--- program.sh
+++ program.sh
@@ -3,6 +3,6 @@
 # Sets internal oscillator to 1MHz and serial uploads to 9600 baud.
 
 "/Applications/Arduino 1.6.4.app/Contents/Java/hardware/tools/avr/bin/avrdude" -C/Applications/Arduino\ 1.6.4.app/Contents/Java/hardware/tools/avr/etc/avrdude.conf -v -patmega328p -cusbtiny -e -Ulock:w:0x3F:m -Uefuse:w:0x06:m -Uhfuse:w:0xD4:m -Ulfuse:w:0x62:m && \
-"/Applications/Arduino 1.6.4.app/Contents/Java/hardware/tools/avr/bin/avrdude" -C"/Applications/Arduino 1.6.4.app/Contents/Java/hardware/tools/avr/etc/avrdude.conf" -v -patmega328p -cusbtiny -Uflash:w:/Users/hermankopinga/Dropbox/Geek/Arduino/hardware/werc/bootloaders/optiboot/optiboot_atmega328_96_1.hex:i -Ulock:w:0x0F:m && \
+"/Applications/Arduino 1.6.4.app/Contents/Java/hardware/tools/avr/bin/avrdude" -C"/Applications/Arduino 1.6.4.app/Contents/Java/hardware/tools/avr/etc/avrdude.conf" -v -patmega328p -cusbtiny -Uflash:w:./optiboot_atmega328_96_1.hex:i -Ulock:w:0x0F:m && \
 # /Applications/Arduino\ 1.6.4.app/Contents/Java/hardware/tools/avr/bin/avrdude -C/Applications/Arduino\ 1.6.4.app/Contents/Java/hardware/tools/avr/etc/avrdude.conf -v -patmega328p -cusbtiny -D -Uflash:w:./radio_receve_test_led.cpp.hex:i && \
 say "Now insert next Atmega"
